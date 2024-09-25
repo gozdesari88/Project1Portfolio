6c543222a32c8e276f0ccf8e216447706c6138e8
GÖZDE SARI <gozdesari88@gmail.com>
2024-09-25 14:16:38 +0300

    ....

---
+++ Project1Portfolio/Controllers/EdducationController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class EdducationController : Controller
+    {
+        // GET: Edducation
+        public ActionResult EdducationList()
+        {
+            return View();
+        }
+        public ActionResult CreateEducation()
+        {
+        return View();  
+        }
+
+    }
+}
---
+++ Project1Portfolio/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using Project1Portfolio.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Project1Portfolio.Controllers
+{
+    public class MessageController : Controller
+    {
+        MyPortfolyoDbEntities3 context = new MyPortfolyoDbEntities3();
+        // GET: Message
+        public ActionResult Inbox()
+        {
+            var values=context.Message.ToList();
+            return View(values);
+
+        }
+        public ActionResult MessageDetails(int id)
+        { 
+        var value=context.Message.Where(x => x.Messageid==id).FirstOrDefault();
+        return View(value);
+        }
+        public ActionResult MessageStatusChangeToTrue(int id)
+        {
+            var value=context.Message.Where(x=> x.Messageid== id).FirstOrDefault(); 
+            value.IsRead = true;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+
+        }
+        public ActionResult MessageStatusChangeToFalse(int id)
+        {
+            var value = context.Message.Where(x => x.Messageid == id).FirstOrDefault();
+            value.IsRead = false;
+            context.SaveChanges();
+            return RedirectToAction("Inbox");
+
+        }
+    }
+}
